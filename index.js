80c2048207c750588bb6ad56936b6ab408fd770b
UGALDEMMJ <151679842+UGALDEMMJ@users.noreply.github.com>
2024-09-26 18:25:20 -0600

    Codigo mee

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const corsOptions={
     }
 }
 
-app.use(cors(corsOptions));
+app.use(cors(cors({origin: '*'})));
 
 app.use("/api/veterinarios", VeterinarioRoutes);
 app.use("/api/pacientes", pacienteRoutes);
